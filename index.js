c164e62d7789002d5346eb49241097eca56977d1
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-31 19:36:13 -0500

    D 81 #100DaysOfCode #freeCodeCamp  #NodeJS  #Backend #MongoDB

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 const mongoose = require("mongoose")
 const userRouter = require("./routes/user")
 const authRouter = require("./routes/auth")
+const productRouter = require("./routes/product")
+const cartRouter = require("./routes/cart")
+const orderRouter = require("./routes/order")
 const port  = process.env.PORT||5000
 
 
@@ -15,6 +18,9 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(express.json())
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
+app.use("/api/product",productRouter)
+app.use("/api/product",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.listen(port,()=>{
     console.log("Backend server is running")
--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,78 @@
+const Product = require("../models/Product")
+const { verifyToken,verifyTokenAndAuthorization,verifyTokenAndAdmin } = require("./verifyToken")
+
 const router = require("express").Router()
 
+//CREATE
+router.post("/",verifyTokenAndAdmin,async  (req,res)=>{
+ const newProduct = new Product(req.body)
+ try{
+    const savedProduct = await newProduct.save()
+    res.status(200).json(savedProduct)
+ }catch(err){
+     res.status(500).json(err)
+ }
+})
+
+//UPDATE
+router.put("/:id",verifyTokenAndAdmin, async (req,res)=>{
+
+    try{
+        const updateProduct = await Product.findByIdAndUpdate(req.params.id,{
+            $set:req.body
+        },{new:true})
+        res.status(200).json(updateProduct)
+    }catch(err){res.status(500).json(err) }
+}
+)
+//DELETE
+
+router.delete("/:id",verifyTokenAndAdmin,async(req,res)=>{
+    try{
+        await Product.findByIdAndDelete(req.params.id)
+        res.status(500).json("Product has been deleted...")
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//GET PRODUCT
+
+router.get("/find/:id",async(req,res)=>{
+    try{
+        const product= await Product.findById(req.params.id)
+        
+        res.status(200).json(product)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+//GET ALL USER
+
+router.get("/",async(req,res)=>{
+    const qNew = req.query.new
+    const qCategory = req.query.category
+
+    try{
+        let product;
+
+        if(qNew){
+            product = await Product.find().sort({createdAt:1}).limit(5)
+        }else if(qCategory){
+            product = await Product.find({
+                categories:{
+                    $in:[qCategory]
+                }
+            })
+        }else{
+            product = await Product.find()
+        }
+
+            res.status(200).json(product)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router
